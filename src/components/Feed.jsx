084cb3bb7fb22b0f8a68cca87d11603cad82cdac
VJS <minsung_96@naver.com>
2022-09-13 23:47:35 +0900

    Video usestate

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography} from '@mui/material'
 import { Sidebar, Videos } from './'
+import { api } from '../utils/api'
 
 const Feed = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState('New')
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        api(`searcj?part=snippet&q=${selectedCategory}`)
+        .then((data) => setVideos(data.items))
+    }, [])
+
     return (
+
         <Stack sx={{ flexDirection: { sx: "column" , md: "row" }}}>
             <Box sx={{ 
                 height: { sx: 'auto', md: '92vh' }, 
                 borderRight: '1px solid #3d3d3d', 
                 px: { sx: 0, md: 2 }
             }}>
-                <Sidebar />
+                <Sidebar 
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={selectedCategory}
+                />
 
                 <Typography className="copyright" variaht="body2" sx={{
                     mt: 1.5, color: '#fff'
@@ -26,13 +40,13 @@ const Feed = () => {
                 <Typography variant="h4" fontWeight="bold" mb={2} sx={{
                     color: 'white'
                 }}>
-                    New <span style={{
+                    {selectedCategory} <span style={{
                         color: '#F31503'
                     }}>
                         videos
                     </span>
                 </Typography>
-                <Videos videos={[]} />
+                <Videos videos={videos} />
             </Box>
         </Stack>
     )
