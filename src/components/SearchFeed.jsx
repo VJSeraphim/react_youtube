ef19a1bb76e8a4c6dcd609cd91eed4ce729d18a8
VJS <minsung_96@naver.com>
2022-09-20 23:59:12 +0900

    Search Page

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Videos } from './'
+import { api } from '../utils/api'
 
-const SearchFeed = () => {
+const Feed = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState('New')
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        api(`searcj?part=snippet&q=${selectedCategory}`)
+        .then((data) => setVideos(data.items))
+    }, [])
+
     return (
-        <div>
-            SearchFeed
-        </div>
+        <Box p={2} sx={{
+            overflowY : 'auto',
+            height: '90vh',
+            flex: 2
+        }}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{
+                color: 'white'
+            }}>
+                {selectedCategory} <span style={{
+                    color: '#F31503'
+                }}>
+                    videos
+                </span>
+            </Typography>
+            <Videos videos={videos} />
+        </Box>
     )
 }
 
-export default SearchFeed
+export default Feed
